b45a729d0acf0501d6dee36d2d4d3dd7a1fc197f
Rojin Shahba <rojin.shahba@ryerson.ca>
2021-09-10 21:04:36 -0400

    Eligible employees to retire

--- Quesries/queries.sql
+++ Quesries/queries.sql
@@ -3,3 +3,25 @@ SELECT * FROM departments;
 SELECT * FROM dept_emp;
 SELECT * FROM dept_manager;
 SELECT * FROM salaries;
+
+-- Retirement eligibility: Born between 1952 and 1955, hired between 1985 and 1988
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Number of employees retiring
+
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Exporting eligible employees to retire
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
